f1611a53547e12b9f48de4c3158a6b048c8c64b1
Deezbec <greger.ert@gmail.com>
2020-05-19 16:49:47 +0300

    Добавил название формы

--- Фоновая 6.1/Form1.Designer.cs
+++ Фоновая 6.1/Form1.Designer.cs
@@ -35,10 +35,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Algerian", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(256, 38);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(341, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(76, 81);
+            this.button1.Size = new System.Drawing.Size(101, 100);
             this.button1.TabIndex = 0;
             this.button1.Text = "A";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,10 +47,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Algerian", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(212, 177);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(283, 218);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 81);
+            this.button2.Size = new System.Drawing.Size(225, 100);
             this.button2.TabIndex = 1;
             this.button2.Text = "B";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,14 +58,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Фоновая 6.2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
